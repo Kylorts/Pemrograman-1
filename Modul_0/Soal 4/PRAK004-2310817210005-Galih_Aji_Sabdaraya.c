ea4c17e538c8fc41b20ea61f91ca95fcab60709d
Galih <galiji57@gmail.com>
2023-09-18 08:22:53 +0700

    first commit

---
+++ Modul_0/Soal 4/PRAK004-2310817210005-Galih_Aji_Sabdaraya.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#    Galih Aji Sabdaraya     #\n");
+    printf("#       2310817210005        #\n");
+    printf("#                            #\n");
+    printf("##############################");
+    return 0;
+}
