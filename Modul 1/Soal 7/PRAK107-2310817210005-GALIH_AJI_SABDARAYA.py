7a274433f9e0a53a5c316b773533e57401f38ccf
Galih Aji Sabdaraya <144782996+Kylorts@users.noreply.github.com>
2023-10-03 02:41:35 +0700

    Add files via upload

---
+++ Modul 1/Soal 7/PRAK107-2310817210005-GALIH_AJI_SABDARAYA.py
@@ -0,0 +1,12 @@
+sisi_a = 4
+sisi_b = 5
+sisi_c = 7
+Biaya_pagar = 85000
+Keliling_tanah = sisi_a + sisi_b + sisi_c
+Biaya_keluar = Keliling_tanah * Biaya_pagar
+print("Diketahui :")
+print(f"Panjang sisi segitiga berturut-turut adalah {sisi_a}, {sisi_b}, dan {sisi_c}")
+print(f"Keliling Tanah Pak Dengklek adalah {Keliling_tanah}")
+print(f"Harga tanah Per Meter adalah {Biaya_pagar}")
+print("Jawaban :")
+print(f"Biaya yang diperlukan Pak Dengklek adalah : Rp {Biaya_keluar}")
