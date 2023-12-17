6523b69a4941b90a0b1bee1b4aac936d6003da2d
Galih Aji Sabdaraya <144782996+Kylorts@users.noreply.github.com>
2023-12-18 04:17:42 +0700

    Add files via upload

---
+++ Modul 0/Soal 2/PRAK002-2310817210005-Galih_Aji_Sabdaraya.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Selamat Pagi, Galih Aji Sabdaraya\n");
+    printf("Selamat Siang, Galih Aji Sabdaraya\n");
+    printf("Selamat Malam, Galih Aji Sabdaraya\n");
+    return 0;
+}
---
+++ Modul 0/Soal 2/PRAK002-2310817210005-Galih_Aji_Sabdaraya.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Galih Aji Sabdaraya")
+print("Selamat Siang, Galih Aji Sabdaraya")
+print ("Selamat Malam, Galih Aji Sabdaraya")
