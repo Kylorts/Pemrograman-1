9974476b44d1a0509ab8278004fe4483376afebd
Galih Aji Sabdaraya <144782996+Kylorts@users.noreply.github.com>
2023-12-18 04:27:33 +0700

    Add files via upload

---
+++ Modul 1/Soal 6/PRAK106-2310817210005-GALIH_AJI_SABDARAYA.py
@@ -0,0 +1,20 @@
+a = 4
+b = 8
+c = 3
+true = 1
+false = 0
+print(f"Variabel a bernilai {a}")
+print(f"Variabel b bernilai {b}")
+print(f"Variabel c bernilai {c}")
+if a == b:
+    print(f"Apakah a sama dengan b ? jawabannya adalah {true}")
+else:
+    print(f"Apakah a sama dengan b ? jawabannya adalah {false}")
+if b > c:
+    print(f"apakah b lebih besar dari c ? jawabannya adalah {true}")
+else:
+    print(f"Apakah b lebih besar dari c ? jawabannya adalah {false}")
+if a != c:
+    print(f"Apakah a tidak sama dengan c ? jawabannya adalah {true}") 
+else:
+    print(f"Apakah a tidak sama dengan c ? jawabannya adalah {false}")
